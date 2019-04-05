a521d368a45bef43e69a477a9cda330dd5bf6ad5
ZGamerYT <47927818+ZGamerYT@users.noreply.github.com>
2019-04-05 21:36:38 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,543 @@
+const { Client, RichEmbed } = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Client();
+const client2 = new Client();
+const client3 = new Client();
+const client4 = new Client();
+const client5 = new Client();
+const client6 = new Client();
+const client7 = new Client();
+const client8 = new Client();
+const client9 = new Client();
+const client10 = new Client();
+const client11 = new Client();
+const client12 = new Client();
+const client13 = new Client();
+const client14 = new Client();
+const client15 = new Client();
+const client16 = new Client();
+const client17 = new Client();
+
+
+
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+
+
+client.on('message',function(message) {
+	let prefix = "!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+		 
+		 
+	client2.on('message',function(message) {
+	let prefix = "!!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client2.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });	 
+		 		 
+		 
+		 
+		 
+	client3.on('message',function(message) {
+	let prefix = "!!!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client3.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });	 
+		 
+		 
+client4.on('message',function(message) {
+	let prefix = "!!!!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client4.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+		 
+		 
+		 
+client5.on('message',function(message) {
+	let prefix = "!!!!!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client5.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+		 
+client6.on('message',function(message) {
+	let prefix = "+";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client6.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+		 
+client7.on('message',function(message) {
+	let prefix = "++";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client7.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+
+	 		 
+client8.on('message',function(message) {
+	let prefix = "+++";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client8.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+	 
+	 
+	 
+	 		 
+client9.on('message',function(message) {
+	let prefix = "++++";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client9.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+	 
+	 		 
+client10.on('message',function(message) {
+	let prefix = "+++++";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client10.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+	 
+	 
+
+
+client11.on('message',function(message) {
+	let prefix = "=";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client11.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+client12.on('message',function(message) {
+	let prefix = "=";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client12.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+	 
+
+client12.on('message',function(message) {
+	let prefix = "=";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client12.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+		 
+		 
+client13.on('message',function(message) {
+	let prefix = "==";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client13.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+		 
+	
+
+		 
+client14.on('message',function(message) {
+	let prefix = "==";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client14.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+		 
+
+
+
+
+
+
+client15.on('message',function(message) {
+	let prefix = "=";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client15.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+client16.on('message',function(message) {
+	let prefix = "=";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client16.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+
+
+client17.on('message',function(message) {
+	let prefix = "=";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
+     client17.on('message', message => {
+         if (message.content === 'كسمكم') {
+               let count = 0;
+               let ecount = 0;
+               for(let x = 0; x < 10000; x++) {
+                 message.channel.send(`:fire: :sob: القلب يحن والعيون تشتاق , انتهي وقت الفقهر وحان وقت الغنا بالكريديت , المهم وخر عني :sunglasses:
+${x}`)
+                   .then(m => {
+                     count++;
+                   })
+
+                 }
+               }
+         });
+	 
+
+
+
+
+
+
+client.login("NTYzMTA2NzM1ODc5ODgwNzc5.XKUgCw.CYvyasJLJJdMPSTtu8BZ5oeZisk")
+client2.login("NTYwMjAxNjMzMTMzMzYzMjQ5.D3wf8w.AGeyJu_mDOyUvR_rJRfV1J7hTmA")
+client3.login("NTYwMjAxNzU4NjI2ODA3ODM4.D32c2Q.fegCkZ6h_gLRwToSw60mXLYvKWI")
+client4.login("NTYwMjAxOTM3MTM2NjQ4MTkz.D32cug.eoqf4ahOs-YGTZTicoTMWo6ZKNA")
+client5.login("NTYwMjAxOTgyODI1MjAxNjg3.D32c6w.aylfR5KS5JdEihxKcIuJ-4k5H8Y")
+client6.login("NTYwMjAyMDIxMzYyNDY2ODI2.D32dCw.dNGol00aPB9Ze_XkPX8GGy_M3JI")
+client7.login("NTYwMjAyMDUwOTk5NDE4OTEz.D32dGg.EFC18rMNjkNW7NMYmq-Nqz2Ae_I")
+client8.login("NTYwMjAyMDkwNzY1MzUyOTYw.D32dJw.rBpfmdSZXPDYDq9HDds_BzggYlk")
+client9.login("NTYwMjAyMTIxNTY4NTgzNjgx.XJ6fDQ.EYpMBiuYOQ-ZOsvgzwhCd2_Ab7c")
+client10.login("NTYwMjAyMjM2ODExMjgwMzk2.XJ6fRA.kqR1fsmGGxjqR2eTgixkIgyFDwQ")
+client11.login("NTYwNTA2ODAzNDkwNjUyMTYw.XJ6fTg.icZq37DbSuQDNhsE4fYWhSigt1Q")
+client12.login("NTYwNTA3MTE4MDQyNzQyODE0.XJ6fVw.PeHkvEwHuF2amcdHg9PG1RoYzfs")
+client13.login("NTYwNTA3MjQ3NzgwNjkxOTY4.XJ6fYA.-0GTkQRUzTEPzaqDc-vPE3J9o1Y")
+client14.login("NTYwNTA3MzM3MTEwOTc4NTYw.XJ6faw.W59KnbyaEe5PubFHPlbJ8w_1HHQ")
+client15.login("NTYwNTA3NDc4MjI0MTQyMzM2.XJ6fdg.26g3OYldrDxusSt1HHmVZdFHemc")
+client16.login("NTYwNTA3NzU3NjI3OTY1NDYw.XJ6fgQ.9-w6-EJ1R78hvJlUBclPUWsZbqI")
+client17.login("NTYwNTA4MjA4MzE4MzgyMTE3.XJ6fiw.fFzHAqptvzQkgFvVnqtKhS5Vzgs")
+
+
+
+
